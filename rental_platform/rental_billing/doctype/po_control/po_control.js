67ea9d392da40c9a86ceba027af36bf1611abd2e
cockroachyie <vivynkrishnaa@gmail.com>
2026-03-26 12:39:08 +0000

    debug: PO Control doctype + billing changes (server scripts missing fixtures)

---
+++ rental_platform/rental_billing/doctype/po_control/po_control.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Vivyn and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("PO Control", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ rental_platform/rental_billing/doctype/po_control/test_po_control.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Vivyn and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPOControl(FrappeTestCase):
+	pass
